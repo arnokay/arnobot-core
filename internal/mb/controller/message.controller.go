dafffa46727a6fe39955ae25cdb717a81ae7b51b
arnokay <arnokay.work@gmail.com>
2025-06-07 00:05:06 +0300

    feat: rename mbtypes to apptype

--- internal/mb/controller/message.controller.go
+++ internal/mb/controller/message.controller.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"fmt"
 
-	"arnobot-shared/mbtypes"
+	"arnobot-shared/apptype"
 	"arnobot-shared/topics"
 
 	"github.com/nats-io/nats.go"
@@ -16,7 +16,7 @@ func (c *MessageController) Connect(conn *nats.Conn) {
 }
 
 func (c *MessageController) NewChatMessage(msg *nats.Msg) {
-	var payload mbtypes.CoreChatMessageNotify
+	var payload apptype.CoreChatMessageNotify
 
 	payload.Decode(msg.Data)
 
